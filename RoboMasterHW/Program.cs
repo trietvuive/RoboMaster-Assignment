e068cf7589c2ac1ebb50e6ca2f7072dad4ecdcf0
Triet Vo <tmv7269@nyu.edu>
2022-04-22 18:36:21 -0400

    Add C#

---
+++ RoboMasterHW/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RoboMasterHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Triet Vo";
+            Console.WriteLine("Hello World! from " + name + " =)");
+        }
+    }
+}
